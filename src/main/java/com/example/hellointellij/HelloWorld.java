e4e103291b1324863db00d131ef8a26f1c89c2f0
Gregory Weiss <gregoryweiss@Gregorys-MacBook-Pro-2.local>
2018-08-09 13:27:32 -0700

    First Commit

---
+++ src/main/java/com/example/hellointellij/HelloWorld.java
@@ -0,0 +1,8 @@
+package com.example.hellointellij;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, IntelliJ!");
+        System.out.println("It's me, Wombat!");
+    }
+}
